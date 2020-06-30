ee5505a08feb515469b6f1b85c2359e0cb2aae5f
Etienne Giroux <etienneg@Etiennes-MacBook-Pro.local>
2020-06-29 20:35:33 -0400

    Initial Setup + Flight list in dropdown

--- public/seat-select/js/seat-select.js
+++ public/seat-select/js/seat-select.js
@@ -1,21 +1,37 @@
-const flightInput = document.getElementById('flight');
-const seatsDiv = document.getElementById('seats-section');
-const confirmButton = document.getElementById('confirm-button');
+const flightInput = document.getElementById("flight");
+const seatsDiv = document.getElementById("seats-section");
+const confirmButton = document.getElementById("confirm-button");
 
-let selection = '';
+let selection = "";
+
+//get all flights numbers and add them to the selector
+function populateFlights() {
+  fetch("/flightList", {
+    method: "GET",
+  })
+    .then((res) => res.json())
+    .then((seatNumbers) => {
+      seatNumbers.forEach((number) => {
+        thisSeat = document.createElement("option");
+        thisSeat.innerText = number;
+        flightInput.appendChild(thisSeat);
+      });
+    });
+}
+populateFlights();
 
 const renderSeats = () => {
-  document.querySelector('.form-container').style.display = 'block';
+  document.querySelector(".form-container").style.display = "block";
 
-  const alpha = ['A', 'B', 'C', 'D', 'E', 'F'];
+  const alpha = ["A", "B", "C", "D", "E", "F"];
   for (let r = 1; r < 11; r++) {
-    const row = document.createElement('ol');
-    row.classList.add('row');
-    row.classList.add('fuselage');
+    const row = document.createElement("ol");
+    row.classList.add("row");
+    row.classList.add("fuselage");
     seatsDiv.appendChild(row);
     for (let s = 1; s < 7; s++) {
       const seatNumber = `${r}${alpha[s - 1]}`;
-      const seat = document.createElement('li');
+      const seat = document.createElement("li");
 
       // Two types of seats to render
       const seatOccupied = `<li><label class="seat"><span id="${seatNumber}" class="occupied">${seatNumber}</span></label></li>`;
@@ -27,17 +43,17 @@ const renderSeats = () => {
     }
   }
 
-  let seatMap = document.forms['seats'].elements['seat'];
+  let seatMap = document.forms["seats"].elements["seat"];
   seatMap.forEach((seat) => {
     seat.onclick = () => {
       selection = seat.value;
       seatMap.forEach((x) => {
         if (x.value !== seat.value) {
-          document.getElementById(x.value).classList.remove('selected');
+          document.getElementById(x.value).classList.remove("selected");
         }
       });
-      document.getElementById(seat.value).classList.add('selected');
-      document.getElementById('seat-number').innerText = `(${selection})`;
+      document.getElementById(seat.value).classList.add("selected");
+      document.getElementById("seat-number").innerText = `(${selection})`;
       confirmButton.disabled = false;
     };
   });
@@ -45,7 +61,7 @@ const renderSeats = () => {
 
 const toggleFormContent = (event) => {
   const flightNumber = flightInput.value;
-  console.log('toggleFormContent: ', flightNumber);
+  console.log("toggleFormContent: ", flightNumber);
   fetch(`/flights/${flightNumber}`)
     .then((res) => res.json())
     .then((data) => {
@@ -62,16 +78,16 @@ const toggleFormContent = (event) => {
 const handleConfirmSeat = (event) => {
   event.preventDefault();
   // TODO: everything in here!
-  fetch('/users', {
-    method: 'POST',
+  fetch("/users", {
+    method: "POST",
     body: JSON.stringify({
-      givenName: document.getElementById('givenName').value,
+      givenName: document.getElementById("givenName").value,
     }),
     headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
   });
 };
 
-flightInput.addEventListener('blur', toggleFormContent);
+flightInput.addEventListener("change", toggleFormContent);
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const { flights } = require('./test-data/flightSeating');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const { flights } = require("./test-data/flightSeating");
 
 const PORT = process.env.PORT || 8000;
 
@@ -12,24 +12,34 @@ const handleFlight = (req, res) => {
   // get all flight numbers
   const allFlights = Object.keys(flights);
   // is flightNumber in the array?
-  console.log('REAL FLIGHT: ', allFlights.includes(flightNumber));
+  console.log("REAL FLIGHT: ", allFlights.includes(flightNumber));
+};
+
+const handleFlightList = (req, res) => {
+  let flightNumbers = Object.keys(flights);
+  console.log(flightNumbers);
+  res.status(200).send(flightNumbers);
 };
 
 express()
   .use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", "*");
     res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
   })
-  .use(morgan('dev'))
-  .use(express.static('public'))
+  .use(morgan("dev"))
+  .use(express.static("public"))
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: false }))
 
   // endpoints
-  .get('/flights/:flightNumber', handleFlight)
-  .use((req, res) => res.send('Not Found'))
+  .get("/flightList", handleFlightList)
+  .get("/flights/:flightNumber", handleFlight)
+  //.get('users/', findUsers)
+  //.post('users/', addUser)
+
+  .use((req, res) => res.send("Not Found"))
   .listen(PORT, () => console.log(`Listening on port ${PORT}`));
